c9289d9bcc622bcf4d9ab02edb02a6fde2c50b88
KannaNoob <damienboran1@gmail.com>
2016-03-19 16:29:22 -0400

    Scripts+Songs

---
+++ Assets/Scripts/Song_Swap.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Song_Swap : MonoBehaviour, IPointerClickHandler
+{
+    private GameObject[] sources;
+    
+    public bool swap;  
+    public AudioClip otherclip;
+    public AudioClip initialclip;
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        sources = GameObject.FindGameObjectsWithTag("Song");
+
+
+        if (swap)
+        {
+            swap = false;
+            foreach (GameObject source in sources)
+            {
+                AudioSource temp = source.GetComponent<AudioSource>();
+                temp.clip = otherclip; temp.Play();
+            }
+        }
+        else {
+            swap = true;
+                foreach (GameObject source in sources)
+                {
+                    AudioSource temp = source.GetComponent<AudioSource>();
+                    temp.clip = initialclip; temp.Play();
+                }
+            }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Volume_Control.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Volume_Control : MonoBehaviour, IPointerClickHandler
+{
+
+    
+    public AudioSource source;
+    public int assign;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        GameObject Temp;
+        switch (assign)
+        {
+            case 0:
+                Temp = GameObject.Find("Vocal");
+                source = Temp.GetComponent<AudioSource>();
+                break;
+            case 1:
+                Temp = GameObject.Find("Snare");
+                source = Temp.GetComponent<AudioSource>();
+                break;
+            case 2:
+                Temp = GameObject.Find("OH");
+                source = Temp.GetComponent<AudioSource>();
+                break;
+            case 3:
+                Temp = GameObject.Find("Kick");
+                source = Temp.GetComponent<AudioSource>();
+                break;
+            case 4:
+                Temp = GameObject.Find("Bass");
+                source = Temp.GetComponent<AudioSource>();
+                break;
+            case 5:
+                Temp = GameObject.Find("Xguitar");
+                source = Temp.GetComponent<AudioSource>();
+                break;
+            case 6:
+                Temp = GameObject.Find("Guitar");
+                source = Temp.GetComponent<AudioSource>();
+                break;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (source.mute)
+            source.mute = false;
+        else
+            source.mute = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
