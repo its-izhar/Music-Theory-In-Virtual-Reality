cc4eca32d88258a1b11092b59a0f19952344c38f
KannaNoob <damienboran1@gmail.com>
2016-03-26 17:05:17 -0400

    Script Fixes + 3d WOHOO

---
+++ Assets/Scripts/Swap_From_3D.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Swap_From_3D : MonoBehaviour, IPointerClickHandler
+{
+    private GameObject[] songs;
+    private GameObject[] zones;
+    public bool is3d = true;
+
+    
+	// Use this for initialization
+	void Start () {
+        zones = GameObject.FindGameObjectsWithTag("Zones");
+
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+
+
+        songs = GameObject.FindGameObjectsWithTag("Song");
+        if (is3d)
+        {
+          //  foreach (GameObject song in songs) 
+               // song.GetComponent<AudioSource>().spatialBlend = 0.0f;
+             
+            foreach(GameObject zone in zones)
+                zone.SetActive(false);
+
+            is3d = false;
+
+
+        }
+        else
+        {
+          //  foreach (GameObject song in songs)
+          //      song.GetComponent<AudioSource>().spatialBlend = 1.0f;
+         
+            foreach (GameObject zone in zones)
+                zone.SetActive(true);
+
+            is3d = true;
+
+
+        }
+    }
+
+        // Update is called once per frame
+        void Update () {
+	
+	}
+}
